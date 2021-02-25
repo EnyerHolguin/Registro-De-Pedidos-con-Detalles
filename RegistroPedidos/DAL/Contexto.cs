11c31aecdbf4fcb4d3f1bb708c067204cab13a5c
Enyer De Jesus Holguin Toribio <enyer_holguin@ucne.edu.do>
2021-02-24 20:22:01 -0400

    Contexto Creados

---
+++ RegistroPedidos/DAL/Contexto.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPedidos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPedidos.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Suplidores> Suplidores { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Ordenes> Ordenes { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Suplidores
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 1,
+                Nombres = "Enyer Holguin"
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 2,
+                Nombres = "Pedro Holguin"
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 3,
+                Nombres = "Leticia Toribio"
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 4,
+                Nombres = "Kennedy Holguin"
+            });
+
+            //Productos--------------------------------------------------
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 1,
+                Descripcion = "jabon",
+                Costo = 100,
+                Inventario = 125
+
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 2,
+                Descripcion = "mentor",
+                Costo = 50,
+                Inventario = 75
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 3,
+                Descripcion = "shampoo",
+                Costo = 110,
+                Inventario = 234
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 4,
+                Descripcion = "salami",
+                Costo = 120,
+                Inventario = 130
+            });
+
+           
+          
+        }
+
+    }
+}
--- RegistroPedidos/Startup.cs
+++ RegistroPedidos/Startup.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RegistroPedidos.BLL;
+using RegistroPedidos.DAL;
 using RegistroPedidos.Data;
 using System;
 using System.Collections.Generic;
@@ -29,6 +32,14 @@ namespace RegistroPedidos
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddDbContext<Contexto>(options =>
+                        options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
+
+                       );
+           
+            services.AddTransient<ProductosBLL>();
+            services.AddTransient<SuplidoresBLL>();
+            services.AddTransient<OrdenesBLL>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
